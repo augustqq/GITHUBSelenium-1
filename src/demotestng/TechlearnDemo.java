9c531d2b628245880572e9d08de55d695054217e
augustqq <toluchurisravani@gmail.com>
2024-10-08 11:19:34 +0530

    techlearn demo

--- src/demotestng/TechlearnDemo.java
+++ src/demotestng/TechlearnDemo.java
@@ -2,18 +2,23 @@ package demotestng;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 
 public class TechlearnDemo {
+	WebDriver driver;
   @Test
-  public void f() {
+  public void google() {
   }
   @BeforeTest
   public void beforeTest() {
+	  driver = new ChromeDriver();
   }
 
   @AfterTest
   public void afterTest() {
+	  driver.quit();
   }
 
 }
